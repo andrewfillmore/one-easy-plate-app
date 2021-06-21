82ef1432f7d62e4aa397247b41b1e5d47cecf05b
Andrew <andrew.fillmore@gmail.com>
2021-06-21 17:37:08 -0500

    Adds initial seed for users and favorites. Associations working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create!([
+  {first_name: "Andrew", last_name: "Fillmore", email: "andrewf@gmail.com", password_digest: "password"},
+  {first_name: "Mimi", last_name: "Reeder", email: "mimir@gmail.com", password_digest: "password"},
+  {first_name: "Winston", last_name: "Strahan", email: "winston@gmail.com", password_digest: "password"},
+  {first_name: "Amy", last_name: "Reeder", email: "amy@gmail.com", password_digest: "password"},
+  {first_name: "Todd", last_name: "Reeder", email: "todd@gmail.com", password_digest: "password"},
+])
+
+
+Favorite.create!([
+  {spoonacular_api_recipe_id: "1101", user_id: 1, comments: "really loved this recipe, but I would cook for about 10 minutes less"},
+  {spoonacular_api_recipe_id: "1202", user_id: 2, comments: "Cook at 350 instead of 375 for the same time"},
+  {spoonacular_api_recipe_id: "3350", user_id: 1, comments: "I would cook this again! I might add some citrus to make it a little brighter"},
+  {spoonacular_api_recipe_id: "2298", user_id: 3, comments: "Use butter instead of oil, and sear at higher temperature"},
+  {spoonacular_api_recipe_id: "4922", user_id: 5, comments: "I did this with rice instead of potatoes and really enjoyed it."}
+])
+
+
