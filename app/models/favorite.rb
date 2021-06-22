7df9f929c348ad478c497c400725668832c55727
Andrew <andrew.fillmore@gmail.com>
2021-06-22 12:40:22 -0500

    Adds association for recipes ghost table

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,9 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  # belongs_to :recipe
+  def recipe
+    response = HTTP.get("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=1&apiKey=307f252bf4564adbbe2109e1ead5a39e&visualizeRecipe")
+
+    recipe_one = response.parse(:json)
+    return recipe_one
+  end
 end
