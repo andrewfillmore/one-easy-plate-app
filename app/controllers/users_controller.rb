3eef6a8b8d8cf8b0cf33d9fc1e3d14c086a1a190
Andrew <andrew.fillmore@gmail.com>
2021-07-01 14:31:36 -0500

    Adds cors to gemfile and uncommented Cors file for login and signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
   def create
     user = User.new(
-      name: params[:name],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
     if user.save
-      render json: { message: "User created successfully" }, status: :created
+      render json: { id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 
   def show
-    user= User.find(params[:id])
+    user = User.find(params[:id])
     render json: user
   end
 
