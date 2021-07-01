ca48d54a13a2e6640571551816572ce583df72b7
Andrew <andrew.fillmore@gmail.com>
2021-07-01 17:52:43 -0500

    double checking the recipes controller.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   def index
     query = params[:query].gsub(" ", "%")
     recipes = HTTP.get("https://api.spoonacular.com/recipes/findByIngredients?ingredients=#{query}&number=5&apiKey=#{Rails.application.credentials.spoonacular_api_key}&visualizeRecipe")
-    # https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=1&apiKey=307f252bf4564adbbe2109e1ead5a39e&visualizeRecipe
+    # https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,flour,sugar&number=1&apiKey=307f252bf4564adbbe2109e1ead5a39e&visualizeRecipe
     
     puts query
     render json: recipes.parse(:json)
